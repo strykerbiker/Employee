855fcd6415f78889287e447ac0d2bf8d7cc1424c
Stryker <srykerpinchin3@gmail.com>
2025-09-19 09:00:52 -0400

    Initial sharing of project

---
+++ Employee.java
@@ -0,0 +1,50 @@
+/**
+ * Consider this incomplete Employee class to answer some questions of your exam
+ *
+ * @author Tassia
+ * @version 0.1
+ */
+public class Employee{
+    private String fullname;
+    private int yearOfBirth;
+    private boolean onLeave;
+    private double hourlyWage;
+    private double unpaidHours;
+
+    public Employee(String fullname, int yearOfBirth, double hourlyWage){
+        this.fullname = fullname;
+        this.yearOfBirth = yearOfBirth;
+        this.hourlyWage = hourlyWage;
+        onLeave = false;
+        unpaidHours = 0.0;
+    }
+    
+    /**
+     * Returns true is an employee is above 16 years old
+     */
+    private int calculateAge(int currentYear){
+        return (currentYear - yearOfBirth);
+    }
+
+    /**
+     * Returns true is an employee is above 16 years old
+     */
+    public boolean canDrive(){
+        int age = calculateAge(2025);
+        // to be completed
+    }
+
+    /*
+     * Returns the net pay for the outstanding unpaid hours
+     */
+    private double calculatePay(){
+        // to be completed
+    }
+
+    /*
+     * Output the payment record and resets unpaid hours
+     */
+    public void paySalary(){
+        // to be completed
+    }
+}
